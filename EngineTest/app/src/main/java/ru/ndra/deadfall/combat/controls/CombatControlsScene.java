233f1364f967c7f5182924280847d654f8b1fde5
alkogolikov <golikov.org@gmail.com>
2017-11-19 14:40:03 +0300

    Кнопки туда-сюда

--- EngineTest/app/src/main/java/ru/ndra/deadfall/combat/controls/CombatControlsScene.java
+++ EngineTest/app/src/main/java/ru/ndra/deadfall/combat/controls/CombatControlsScene.java
@@ -3,13 +3,13 @@ package ru.ndra.deadfall.combat.controls;
 import ru.ndra.engine.di.OnCreate;
 import ru.ndra.engine.event.EventManager;
 import ru.ndra.engine.gameobject.Scene;
-import ru.ndra.engine.gameobject.Sprite;
 
 public class CombatControlsScene extends Scene implements OnCreate {
 
     private final EventManager eventManager;
 
     private Button moveBackButton;
+    private Button moveForthButton;
 
     public CombatControlsScene(EventManager eventManager) {
         super();
@@ -19,9 +19,20 @@ public class CombatControlsScene extends Scene implements OnCreate {
     @Override
     public void update(float dt) {
         super.update(dt);
-        if(this.moveBackButton.isPressed()) {
+
+        boolean pressed = false;
+
+        if (this.moveBackButton.isPressed()) {
             eventManager.trigger("control/move-backward");
-        } else {
+            pressed = true;
+        }
+
+        if (this.moveForthButton.isPressed()) {
+            eventManager.trigger("control/move-forward");
+            pressed = true;
+        }
+
+        if(!pressed) {
             eventManager.trigger("control/move-stop");
         }
     }
@@ -39,37 +50,11 @@ public class CombatControlsScene extends Scene implements OnCreate {
         this.moveBackButton.position.x = -700;
         this.moveBackButton.isButton = true;
 
-
-
-        /*moveBackButton.events.on("touch", event -> {
-
-            TouchEvent touchEevent = (TouchEvent) event;
-
-            if (touchEevent.action == MotionEvent.ACTION_DOWN || touchEevent.action == MotionEvent.ACTION_POINTER_DOWN) {
-                eventManager.trigger("control/move-backward");
-            }
-            if (touchEevent.action == MotionEvent.ACTION_UP) {
-                eventManager.trigger("control/move-stop");
-            }
-        }); */
-
-        // Кнопка "Вперед"
-        /*class MoveForthButton extends Sprite {
-            public void onTouch(TouchEvent event) {
-                if (event.action == MotionEvent.ACTION_DOWN || event.action == MotionEvent.ACTION_POINTER_DOWN) {
-                    eventManager.trigger("control/move-forward");
-                }
-                if (event.action == MotionEvent.ACTION_UP) {
-                    eventManager.trigger("control/move-stop");
-                }
-            }
-        };
-        MoveForthButton moveForthButton = (MoveForthButton) this.add(MoveForthButton.class);
-        moveForthButton.width = 200;
-        moveForthButton.height = 200;
-        moveForthButton.position.y = -200;
-        moveForthButton.position.x = -300;
-        moveForthButton.zIndex = 1000;
-        moveForthButton.isButton = true; */
+        this.moveForthButton = (Button) this.add(Button.class);
+        this.moveForthButton.width = 200;
+        this.moveForthButton.height = 200;
+        this.moveForthButton.position.y = -200;
+        this.moveForthButton.position.x = -300;
+        this.moveForthButton.zIndex = 1000;
     }
 }
