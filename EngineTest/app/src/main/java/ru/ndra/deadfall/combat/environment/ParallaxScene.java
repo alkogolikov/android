6a1613c92eaeb9a962046d582eba6e1f1e9c4aa7
alkogolikov <golikov.org@gmail.com>
2017-11-19 15:30:08 +0300

    Перемещение фона за камерой

--- EngineTest/app/src/main/java/ru/ndra/deadfall/combat/environment/ParallaxScene.java
+++ EngineTest/app/src/main/java/ru/ndra/deadfall/combat/environment/ParallaxScene.java
@@ -2,6 +2,7 @@ package ru.ndra.deadfall.combat.environment;
 
 import ru.ndra.engine.event.Event;
 import ru.ndra.engine.event.EventManager;
+import ru.ndra.engine.gameobject.GameObject;
 import ru.ndra.engine.gameobject.Scene;
 
 public class ParallaxScene extends Scene {
@@ -12,11 +13,13 @@ public class ParallaxScene extends Scene {
         super();
         eventManager.on("combat/camera-position", (Event event) -> {
             this.offsetX = event.paramsFloat.get("x");
+            for (GameObject sprite : this.children) {
+                ((ParallaxSprite) sprite).offset = this.offsetX;
+            }
         });
     }
 
     public void addParallax(final String texture, float height, final float bottom, final float speed, float selfSpeed, boolean onTop) {
-
         ParallaxSprite sprite = (ParallaxSprite) this.add(ParallaxSprite.class);
         sprite.setTexture(texture);
         sprite.height = height;
